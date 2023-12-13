77e48c2043147fe95d2eb0b7f30ce6bf1ce7d0b9
sravanthi <sravssmily53@gmail.com>
2023-12-13 12:18:03 +0530

    first commit

---
+++ src/main/java/TestingProject_6/TestingProject_6/App.java
@@ -0,0 +1,13 @@
+package TestingProject_6.TestingProject_6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"e93ce202-7b0b-410b-82dd-0f25bad9ee54","feature":"E-commerce Website Filter","scenario":"User applies filter to search for products","start":1702358085362,"group":1,"content":"","tags":"","end":1702358135578,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
